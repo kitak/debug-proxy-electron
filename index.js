4745e48ead45cd2c7939ffd7e2de85d59e3117ca
Keisuke KITA <kei.kita2501@gmail.com>
2015-10-01 21:02:47 +0900

    Error handling

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var url = require('url');
 
 var proxy = httpProxy.createProxyServer({});
 
+proxy.on('error', function (err, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Something wrong: '+err);
+});
+
 var server = http.createServer(function(req, res) {
   var parsedUrl = url.parse(req.url);
   var protocol = parsedUrl.protocol;
